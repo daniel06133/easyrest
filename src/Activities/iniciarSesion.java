cbf0e458430629eb016f184c44e861a2309acc8e
CatheHuguet <cathe.huguet@gmail.com>
2015-11-10 17:36:40 -0300

    Pantalla Iniciar Sesion

--- src/Activities/iniciarSesion.java
+++ src/Activities/iniciarSesion.java
@@ -1,5 +1,32 @@
 package Activities;
 
-public class iniciarSesion {
+import android.R;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class iniciarSesion extends Activity implements OnClickListener
+{
+	private EditText txtNombreUsuario;
+	private EditText txtContrasena;
+	private Button btnIniciarSesion;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.iniciarSesion);
+	};
+	
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
