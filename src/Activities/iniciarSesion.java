26e1d7143e523a61377484ca0b949a01bb29a69a
DANIEL\Danielito <danielito_m06@hotmail.com>
2015-11-10 20:15:33 -0300

    IniciarSesion Update

--- src/Activities/iniciarSesion.java
+++ src/Activities/iniciarSesion.java
@@ -1,14 +1,20 @@
 package Activities;
 
+import java.util.List;
+
 import com.example.proyectomoviles.*;
+
 import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class IniciarSesion extends Activity implements OnClickListener
+public class IniciarSesion extends Activity implements  android.view.View.OnClickListener
 {
 	private EditText txtNombreUsuario;
 	private EditText txtContrasena;
@@ -19,17 +25,41 @@ public class IniciarSesion extends Activity implements OnClickListener
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.iniciarsesion);
 
+		btnIniciarSesion = (Button) findViewById(R.id.btnIniciarSesion);
+		txtNombreUsuario = (EditText) findViewById(R.id.txtNombreUsuario);
+		txtContrasena = (EditText) findViewById(R.id.txtContrasena);
+		
+		btnIniciarSesion.setOnClickListener(this);
 		
 	};
-	
+
 	@Override
-	public void onClick(DialogInterface dialog, int which) {
+	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		Intent intent = null;
 		
+		if(v.getId() == R.id.btnIniciarSesion)
+			intent = new Intent((this),Mesas.class);
+		
+		
+		if (isIntentAvailable(intent)) {
+			//Aquí validar usuario y contraseña contra la base de datos.
+			startActivity(intent);
+		} else {
+			Toast toast = Toast.makeText(this, "Intent can't be handled",
+					Toast.LENGTH_SHORT);
+			toast.show();
+		}
 	}
 	
+	
+	private boolean isIntentAvailable(Intent intent) {
+		final PackageManager packageManager = getPackageManager();
+		List<ResolveInfo> list = packageManager.queryIntentActivities(intent,
+				PackageManager.MATCH_DEFAULT_ONLY);
+		return list.size() > 0;
+	}
 
 }
